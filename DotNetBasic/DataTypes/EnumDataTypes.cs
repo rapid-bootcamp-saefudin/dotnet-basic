28358f6c079ee6b25cb94ba35038d9cd81da0401
Muhammad Saefudin <itsapud@gmail.com>
2023-01-03 10:12:53 +0700

    DotNetBasic Day 2 - add Enum Data Type & update Main Data Type

---
+++ DotNetBasic/DataTypes/EnumDataTypes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    public enum EnumDays
+    {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+
+    public enum Colors { Red = 1, Green = 2, Blue = 4, Yellow = 8 };
+
+    public enum EnumFileMode
+    {
+        CreateNew = 1,
+        Create = 2,
+        Open = 3,
+        OpenOrCreate = 4,
+        Truncate = 5,
+        Append = 6
+    }
+
+    public enum ApprovalStep
+    {
+        Submitted = 0,
+        Requested = 1,
+        Readed = 2,
+        Approved = 3,
+        Rejected = 4
+    }
+
+    public class EnumDataType
+    {
+        public EnumDataType()
+        {
+        }
+    }
+
+}
--- DotNetBasic/DataTypes/MainDataTypes.cs
+++ DotNetBasic/DataTypes/MainDataTypes.cs
@@ -6,11 +6,69 @@ using System.Threading.Tasks;
 
 namespace DataTypes
 {
-    public class DataTypes
+    public struct Coords
+    {
+        public int x, y;
+        public Coords(int p1, int p2)
+        {
+            x = p1;
+            y = p2;
+        }
+
+        public override string ToString()
+        {
+            return "Coord (" + x + ", " + y + ")";
+        }
+    }
+
+    public class MainDataType
     {
         public static void Main()
         {
-            // Declaration with initializers (four examples):
+            ContohEnum();
+        }
+
+
+        #region Contoh Enum
+        public static void ContohEnum()
+        {
+            Type weekDays = typeof(EnumDays);
+            foreach (var item in Enum.GetNames(weekDays))
+            {
+                Console.WriteLine("Days: {0}", item);
+            }
+
+            Type fieldModel = typeof(EnumFileMode);
+            foreach (var item in Enum.GetNames(fieldModel))
+            {
+                Console.WriteLine("FieldMode : {0}", item);
+            }
+
+            Colors myColors = Colors.Red | Colors.Blue | Colors.Yellow;
+            Console.WriteLine();
+            Console.WriteLine("myColors holds a combination of colors. Namely: {0}", myColors);
+            Console.WriteLine("Color Red: {0}, GetName: {1}", Colors.Red, Enum.GetName(Colors.Red));
+
+            Console.WriteLine("Status Approved: {0}, Names: {1}", ApprovalStep.Approved, Enum.GetName(ApprovalStep.Approved));
+
+        }
+        #endregion
+
+        #region Contoh Coord
+        public static void ContohCoord()
+        {
+            Coords point1 = new Coords(2, 5);
+            Console.Write("Point 1 : " + point1);
+            Console.WriteLine();
+
+            Coords point2 = new Coords(5, 5);
+            Console.Write("Point 2 : " + point2);
+        }
+        #endregion
+
+        #region Contoh Data Type
+        public static void ContohDataType()
+        {
             char firstLetter = 'C';
             var limit = 3;
             int[] source = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 13, 15, 18, 21 };
@@ -25,8 +83,8 @@ namespace DataTypes
             }
 
             var query2 = from item in source
-                        where item %2 == 1
-                        select item;
+                         where item % 2 == 1
+                         select item;
 
             Console.WriteLine("Write item with odd : " + query);
             foreach (var item in query)
@@ -35,8 +93,8 @@ namespace DataTypes
             }
 
             var query3 = from item in source
-                        where item %2 == 0
-                        select item;
+                         where item % 2 == 0
+                         select item;
 
             Console.WriteLine("Write item with even : " + query);
             foreach (var item in query)
@@ -45,8 +103,8 @@ namespace DataTypes
             }
 
             var query4 = from item in source
-                        where item % 3 == 0
-                        select item;
+                         where item % 3 == 0
+                         select item;
 
             Console.WriteLine("Write item with multiple 3 : " + query);
             foreach (var item in query)
@@ -54,5 +112,9 @@ namespace DataTypes
                 Console.WriteLine("item value : " + item);
             }
         }
+        #endregion
     }
+    //public class DataTypes
+    //{
+    //}
 }
