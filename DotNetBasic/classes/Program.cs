9c0b61b16ce4d8f6615d08a44cbbd5b92e950a3e
Muhammad Saefudin <itsapud@gmail.com>
2023-01-04 03:55:30 +0700

    Object-oriented

---
+++ DotNetBasic/classes/Program.cs
@@ -0,0 +1,7 @@
+﻿using Classes;
+
+var account = new BankAccount("Muhammad Saefudin", 14000000000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+var account2 = new BankAccount("Apud", 16500000000);
+Console.WriteLine($"Account2 {account2.Number} was created for {account2.Owner} with {account2.Balance} initial balance.");
---
+++ DotNetBasic/classes/Transaction2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    public class Transaction2
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+        public Transaction2(decimal amount, DateTime date, string note)
+        {
+            Amount = amount;
+            Date = date;
+            Notes = note;
+        }
+    }
+}
