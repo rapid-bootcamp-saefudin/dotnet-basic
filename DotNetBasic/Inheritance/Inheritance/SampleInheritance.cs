d1936f5deb7ca54c594f8697043c10e0291e2122
Muhammad Saefudin <itsapud@gmail.com>
2023-01-04 13:42:27 +0700

    Add Sample Inheritance

---
+++ DotNetBasic/Inheritance/Inheritance/SampleInheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance.Inheritance
+{
+    public class SampleInheritance
+    {
+        public class A
+        {
+            private int _value = 10;
+
+            public class B : A
+            {
+                public int GetValue()
+                {
+                    return _value;
+                }
+            }
+        }
+
+        public class C : A
+        {
+            //    public int GetValue()
+            //    {
+            //        return _value;
+            //    }
+        }
+    }
+}
---
+++ DotNetBasic/Inheritance/MainInheritance.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Inheritance.Inheritance.SampleInheritance;
+
+namespace Inheritance
+{
+    public class MainInheritance
+    {
+        public static void Main(string[] args)
+        {
+            var b = new A.B();
+            Console.WriteLine(b.GetValue());
+        }
+    }
+    // The example displays the following output:
+    //       10    }
+}
